6c6de76077f5f5fbf68584a54564af3e124508ce
plb9772 <36765599+plb9772@users.noreply.github.com>
2019-12-17 09:35:19 -0800

    Moving Block implementation

---
+++ functions/MPC_MovingBlock.m
@@ -0,0 +1,149 @@
+function [feas, xOpt, uOpt, predErr, x_pred_l, x_pred_f, u_pred_l, u_pred_f] = MPC_MovingBlock(x0_l, x0_f,param,MODEL,...
+        slope_,radius_,DPspeed_,maxspeed_,p_sampled)
+    
+%% performs MPC
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Inputs 
+% x0_l          initial position of leading train
+% x0_f          initial position of following train
+% param         struct with param
+% MODEL         indicator which model is used
+% slope_        function handle for slope function
+% radius_       function handle for radius_ function
+% limspeed_     function handle for limspeed_ function
+% maxspeed_     function handle for maxspeed_ function
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Outputs
+% feas       vector of logicals that indicate the feasibility of the problem at each simulation step
+% xOpt       array of the optimal closed-loop trajectory 
+% uOpt       array of the optimal input sequence 
+% predErr    array of the l2-norm of the difference between the openloop predictions 
+%            and the closed-loop trajectory for each state from simulation timestep 
+% x_pred_l   array of predicted trajectories for leading train
+% x_pred_f   array of predicted trajectories for following train
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+% midterm = 1 ;
+% paper = 0 ;
+% 
+% if MODEL == midterm
+%     % number of states
+%     nx = 2 ;
+% elseif MODEL == paper
+%     % number of states
+%     nx = 3 ;
+% else
+%     error('MODEL is not assigned correctly')
+% end
+% ( currently not needed ) 
+
+% turns off fmincon warning
+warning('off','MATLAB:nearlySingularMatrix')
+
+
+% initialisation of storage vectors
+feas_l = [] ;
+feas_f = [] ;
+
+xOpt_l = x0_l;
+xOpt_f = x0_f;
+
+uOpt_l = [] ;
+uOpt_f = [] ;
+
+x_pred_l = [] ;
+x_pred_f = [] ;
+u_pred_l = [] ;
+u_pred_f = [] ;
+
+% pred error not implemented yet
+predErr = [] ;
+% predErr_l = zeros(nx,1);
+% predErr_f = zeros(nx,1);
+
+% initialisation for apriori estimation before first step
+uOpt_l_t = 1*1.8449e+05*ones(1,param.Np) ;
+uOpt_f_t = 1*1.8449e+05*ones(1,param.Np) ;
+
+% indicator of MPC iteration
+i = 1 ;
+
+while xOpt_l(1,end) < p_sampled(1,end)
+    tic
+    % apriori estimation
+    [xbar_l, ubar_l] = a_priori_estimation(x0_l, uOpt_l_t, param, MODEL, ...
+        slope_, radius_, DPspeed_, maxspeed_);
+    [xbar_f, ubar_f] = a_priori_estimation(x0_f, uOpt_f_t, param, MODEL, ...
+        slope_, radius_, DPspeed_, maxspeed_);
+    
+    % execute finite time horizon optimization
+    [feas_l_t, xOpt_l_t, uOpt_l_t, JOpt_l_t] = cftoc_leadingTrain(x0_l, ... 
+       xbar_l, ubar_l, MODEL, param, slope_,radius_,DPspeed_,maxspeed_) ;
+    
+   if feas_l_t ~= 1
+        xOpt_l = [];
+        xOpt_f = [];
+        uOpt_l = [];
+        uOpt_f = [];
+%         predErr_l = [];
+%         predErr_f = [];
+        feas_l = [feas_l, false]; 
+        disp('No feasible solution found for leading train.')
+        break;
+   else
+       feas_l = [feas_l, true];
+       % store current prediction
+       x_pred_l{i} = xOpt_l_t ;
+       u_pred_l{i} = uOpt_l_t ;
+
+        % execute finite time horizon optimization for following train
+        [feas_f_t, xOpt_f_t, uOpt_f_t, JOpt_f_t] = cftoc_followingTrainMB(x0_f, ... 
+            xbar_f, ubar_f, xOpt_l_t(:,1), MODEL, param, slope_,radius_,DPspeed_,maxspeed_) ;
+ 
+       if feas_f_t ~= 1
+            xOpt_l = [];
+            xOpt_f = [];
+            uOpt_l = [];
+            uOpt_f = [];
+%             predErr_l = [];
+%             predErr_f = [];
+            feas_f = [feas_f, false];
+            disp('No feasible solution found for following train.')
+            break;
+       else
+            feas_f = [feas_f, true];
+            % optimal closed loop trajectory
+            xOpt_l = [xOpt_l, xOpt_l_t(:,2)];
+            xOpt_f = [xOpt_f, xOpt_f_t(:,2)];
+            uOpt_l = [uOpt_l, uOpt_l_t(1,1)];
+            uOpt_f = [uOpt_f, uOpt_f_t(1,1)];
+            % new initial position
+            x0_l = xOpt_l_t(:,2) ;
+            x0_f = xOpt_f_t(:,2) ;
+            % store current prediction
+            x_pred_f{i} = xOpt_f_t ;
+            u_pred_f{i} = uOpt_f_t ;
+       end
+    disp('MPC iteration')
+    disp(i)
+    disp('leading train position')
+    disp(x0_l)
+    disp('following train position')
+    disp(x0_f)
+    
+    % print results of current iteration
+    plot_results([], xOpt_l, uOpt_l, xOpt_f, uOpt_f, predErr, x_pred_l, x_pred_f, p_sampled, DPspeed_, maxspeed_,47)
+
+    % next MPC step   
+    i = i + 1 ;
+    toc
+    
+    end
+    
+xOpt{1} = xOpt_l ;
+xOpt{2} = xOpt_f ;
+
+uOpt{1} = uOpt_l ;
+uOpt{2} = uOpt_f ;
+
+end
---
+++ functions/cftoc_followingTrainMB.m
@@ -0,0 +1,185 @@
+function [feas_f, xOpt_f, uOpt_f, JOpt_f] = cftoc_followingTrainMB(x0, ... 
+     xbar, ubar, xleader, MODEL, param, slope_,radius_,DPspeed_,maxspeed_)
+%% solves CFTOC problem for the leading train
+%% Inputs
+% x0        initial state
+% xbar      vector containing the estimated states, a priori estimation
+% ubar      vector containing the inputs used for the estimation
+% xleader   leader state at time t (only first one needed!)
+%           
+% MODEL     indicator which model is used  
+% param     struct containing all the system + MPC parameters 
+% slope_     
+% radius_   
+% limspeed_ 
+% maxspeed_
+% vOpt_DP   data from DP, optimal velocity at sample points
+%% Outputs
+% feas_l    information if CFTOC problem is feasible
+% xOpt_l    optimal sequence of states 
+% uOpt_l    optimal input sequence 
+% Jopt_l    optimal cost function
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% for decision which MODEL is used
+midterm = 1 ;
+paper = 0 ;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% MODEL == midterm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if MODEL == midterm
+    %% extract paramters 
+    Kv_f = param.Kv_f; % gain for penalizing deviation from max speed
+    Kd = param.Kd;   % gain for penalizing deviation from desired distance
+    M = param.M;
+    Np = param.Np;  % MPC horizon 
+    g = param.g;
+    d_des = param.d_des;  % desired distance to leader
+    d_min = param.d_min;
+    a_l = param.a_l; % max accelerations for terminal constraint on distance
+    a_f = param.a_f; % -> have to tune this
+                     % (or leave out this constraint)
+    L = param.L ; % length of trains
+    %% define optimization variable for state
+    x = sdpvar(2,Np+1);
+    assign(x,xbar);
+    %% define optimization variable for input 
+    u = sdpvar(1,Np);
+    assign(u,ubar);
+    %% define objective function
+    objective = 0;
+    % initialize distance to leader in each time step
+    %d = zeros(1,Np+1);
+    for k = 1:Np+1 % sum from t to t+Np, because of matlab indexing -> from 1 to Np+1  
+     %   d(1,k) = x(1,k) - xbar_l(1,k) - L;  % current distance to leader
+        objective = objective + Kd * norm(xleader(1) - x(1,k) - L - d_des) + ...
+            Kv_f * norm(x(2,k) - maxspeed_(x(1,k)));   % used in paper  
+    end
+    %% define constraints
+    constraints = [];
+    for i = 1:Np % constraints for time steps t to t+Np-1, have terminal constraints for timestep t+Np
+        constraints = [constraints x(:,i+1) == train_dynamics_midterm(x(:,i),...  % (11d)
+            u(1,i),param,slope_,radius_,DPspeed_,maxspeed_)... % with estimated values  alternatively: x(:,i+1) == train_dynamics(x(:,i), u(1,i), param) used in paper
+        d_min <= xleader(1) - x(1,i) - L ...  % (11e)
+        0 <= x(2,i+1) <= maxspeed_(xbar(1,i+1)) ... % (11f)
+        % can't use (11g) without force in states
+        -M*g*param.mumax*1e-4  <= u(1,i)*1e-4 <= M*g*param.mumax*1e-4]; % midterm equivalent of (11h)
+        %-Pbr <= x(2,i+1) * u(1,i) <= Pdr];
+    end
+    % initial constraint
+    constraints = [constraints x0 == x(:,1)];  % (11j)
+    % terminal constraint on terminal velocity from precomputed DP
+    constraints = [constraints 0 <= x(2,Np+1) <= DPspeed_(xbar(1,Np+1))];  % (11k)
+    % terminal constraint on distance
+    constraints = [constraints d_min <= xleader(1) - x(1,Np+1) - L ...  % (11l)
+                    d_min <= xleader(1) - x(1,Np+1) - L  - (x(2,Np+1)^2/(2*a_f))];  % (11m)
+    %% Optimize
+    options = sdpsettings('verbose',0,'usex0',1,'solver','fmincon','fmincon.MaxIter',500000,...
+        'fmincon.MaxFunEvals',5000000,'fmincon.TolFun',1e-06,'fmincon.TolFunValue',1e-06,'fmincon.TolCon',1e-03);
+    sol = optimize(constraints, objective, options);
+    error = sol.problem;
+
+    if error == 0
+        % correct solution found
+        feas_f = 1;
+        xOpt_f = double(x);
+        uOpt_f = double(u);
+        JOpt_f = double(objective);
+    else
+        feas_f = 0;
+        xOpt_f = [];
+        uOpt_f = [];
+        JOpt_f = [];
+end % model == midterm
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% MODEL == paper %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% model selection
+elseif MODEL == paper 
+    %% get paramters 
+    delta_t = param.delta_t;
+    Kv_f = param.Kv_f; % gain for penalizing deviation from max speed
+    Kj_f = param.Kj_f; % gain for penalizing jerk
+    Kd = param.Kd;   % gain for penalizing deviation from desired distance
+    M = param.M;
+    Np = param.Np;
+    jmax = param.jmax;
+    abr = param.abr;
+    adr = param.adr; 
+    Pbr = param.Pbr;
+    Pdr = param.Pdr;
+    d_des = param.d_des;  % desired distance to leader
+    d_min = param.d_min;
+    a_l = param.a_l; % max accelerations for terminal constraint on distance
+    a_f = param.a_f; % -> have to tune this
+                     % (or leave out this constraint)
+    %% Define optimization variable for state
+    x = sdpvar(3,Np+1);
+    assign(x,xbar);
+    %% Define optimization variable for input 
+    u = sdpvar(1,Np);
+    assign(u,ubar);
+    %% Define objective function
+    objective = 0;
+    % initialize distance to leader in each time step
+    d = zeros(Np+1);
+    for k = 1:Np+1  % sum from t to t+Np, because of matlab indexing -> from 1 to Np+1  
+        d(k) = x(1,k) - xleader - L;  % current distance to leader
+        objective = objective + Kd * norm(d(k) - d_des) + ...
+            Kv_f * norm(x(2,k) - maxspeed_(x(1,k)));   % used in paper  
+    end
+    for k = 1:Np-1
+        % penalize jerk
+        objective = objective + Kj_l * norm((x(3,k+1) - x(3,k)) / (delta_t* M)); % (11c)     
+    end
+    %% Define constraints
+    constraints = [];
+    for i = 1:Np  % constraints for time steps t to t+Np-1, have terminal constraints for timestep t+Np
+        constraints = [constraints x(:,i+1) == train_dynamics(x(:,i), u(1,i), param,slope_,radius_,DPspeed_,maxspeed_)... %  with estimated values  alternatively: x(:,i+1) == train_dynamics(x(:,i), u(1,i), param) used in paper
+        d_min <= xleader - x(1,i) - L ...  % (11e)
+        0 <= x(2,i+1) <= maxspeed_(xbar(1,i+1)) ... % (11f)
+        -jmax <= (x(3,k+1) - x(3,k))/ (M * delta_t) <= jmax ... %(11g)
+        -M * abr <= u(1,i) <= M * adr... % (11h)
+        -Pbr <= x(2,i+1) * u(1,i) <= Pdr];  % (11i)
+    end
+    % initial constraint
+    constraints = [constraints x0 == x(:,1)];  % (11j)
+    % terminal constraint on terminal velocity from precomputed DP
+    constraints = [constraints 0 <= x(2,Np+1) <= DPspeed_(xbar(1,Np+1))];  % (11k)
+    % terminal constraints on distance
+    constraints = [constraints d_min <= d(Np+1) ...  % (11l)
+                    d_min <= d(Np+1) - (x(2:Np+1)^2/(2*a_f))];  % (11m)
+    %% Optimize
+        options = sdpsettings('verbose',0,'usex0',1,'solver','fmincon','fmincon.MaxIter',500000,...
+            'fmincon.MaxFunEvals',5000000,'fmincon.TolFun',1e-05,'fmincon.TolFunValue',1e-05);
+
+    sol = optimize(constraints, objective, options);
+
+    error = sol.problem;
+
+    if error == 0
+        % correct solution found
+        feas_f = 1;
+        xOpt_f = double(x);
+        uOpt_f = double(u);
+        JOpt_f = double(objective);
+
+    else
+        feas_f = 0;
+        xOpt_f = [];
+        uOpt_f = [];
+        JOpt_f = [];
+    end  % model == paper
+
+    else
+        error('MODEL is not assigned correctly')
+    end % model selection
+
+end % function
